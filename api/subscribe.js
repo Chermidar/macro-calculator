c15d84b14e7370098e1fbd57596438a09855ef16
Carlos Hermida <105751101+Chermidar@users.noreply.github.com>
2025-01-03 22:28:08 +0100

    Update subscribe.js

--- api/subscribe.js
+++ api/subscribe.js
@@ -1,4 +1,3 @@
-
 export default function handler(req, res) {
     if (req.method === 'POST') {
         const apiKey = 'pk_ae2f5d0763e6277fd8e1b6f6afd5de37a2';
@@ -10,14 +9,16 @@ export default function handler(req, res) {
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({ profiles: [req.body] }),
+            body: JSON.stringify({
+                profiles: [req.body] // Se envía como un array
+            }),
         })
-            .then(response => response.json())
-            .then(data => res.status(200).json({ success: true, data }))
-            .catch(error => {
-                console.error(error);
-                res.status(500).json({ success: false, error: 'Failed to subscribe' });
-            });
+        .then(response => response.json())
+        .then(data => res.status(200).json({ success: true, data }))
+        .catch(error => {
+            console.error(error);
+            res.status(500).json({ success: false, error: 'Failed to subscribe' });
+        });
     } else {
         res.status(405).json({ error: 'Method Not Allowed' });
     }
